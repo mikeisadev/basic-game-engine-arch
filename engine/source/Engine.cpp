16f90e9f261382a767cc5c3d332d110cdff5da6b
Michele <michelework02@gmail.com>
2026-09-14 23:11:00 +0200

    Implementing file system login for cross platform (windows, linux and macos) to load textures and game assets

--- engine/source/Engine.cpp
+++ engine/source/Engine.cpp
@@ -191,6 +191,11 @@ namespace eng
         return m_renderQueue;
     }
 
+    FileSystem& Engine::GetFileSystem() 
+    {
+        return m_fileSystem;
+    }
+
     void Engine::SetScene(Scene* scene)
     {
         m_currentScene.reset(scene);
