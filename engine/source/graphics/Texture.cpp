356d682975170a8e7ea1c53d67af6c519b1889ab
Michele <michelework02@gmail.com>
2026-09-16 09:16:46 +0200

    Improving loading of textures and shaders. Using FileSystem class to load shaders also to achieve better looking code.

--- engine/source/graphics/Texture.cpp
+++ engine/source/graphics/Texture.cpp
@@ -1,23 +1,15 @@
 #include "graphics/Texture.h"
+#include "Engine.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
 
 namespace eng
 {
     Texture::Texture(int width, int height, int numChannels, unsigned char* data)
         : m_width(width), m_height(height), m_numChannels(numChannels)
     {
-        glGenTextures(1, &m_textureID);
-        glBindTexture(GL_TEXTURE_2D, m_textureID);
-
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-
-        glGenerateMipmap(GL_TEXTURE_2D);
-
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
+        Init(width, height, numChannels, data);
     }
 
     Texture::~Texture() 
@@ -31,5 +23,46 @@ namespace eng
     GLuint Texture::GetID() const
     {
         return m_textureID;
+    }
+
+    void Texture::Init(int width, int height, int numChannels, unsigned char* data)
+    {
+        glGenTextures(1, &m_textureID);
+        glBindTexture(GL_TEXTURE_2D, m_textureID);
+
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    }
+
+    std::shared_ptr<Texture> Texture::Load(const std::string& path)
+    {
+        int width, height, numChannels;
+
+        auto& fs = Engine::GetInstance().GetFileSystem();
+        auto fullPath = fs.GetAssetsFolder() / path;
+
+        if (!std::filesystem::exists(fullPath))
+        {
+            return nullptr;
+        }
+
+        std::shared_ptr<Texture> result;
+
+        unsigned char* data = stbi_load(fullPath.string().c_str(), &width, &height, &numChannels, 0);
+
+        if (data)
+        {
+            result = std::make_shared<Texture>(width, height, numChannels, data);
+            stbi_image_free(data);
+        }
+
+        return result;
     }
 }
