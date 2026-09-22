4c3e469787715eecc17b6d6c855b0d2f8d09ddbe
Michele <michelework02@gmail.com>
2026-09-22 21:06:54 +0200

    Implementing miniaudio library and audio logic

---
+++ engine/source/scene/components/AudioListenerComponent.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "scene/Component.h"
+
+namespace eng
+{
+    class AudioListenerComponent : public Component
+    {
+        COMPONENT(AudioListenerComponent)
+
+        public:
+            void Update(float deltaTime) override;
+    };
+}
